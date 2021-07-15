67961247e7e50ae0773a246c554090832f60f8c5
unknown <yenjyao@gmail.com>
2021-07-15 17:10:01 +0100

    Added controller

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const todoController = require('./controllers/todoController') 
+const app = express()
+const port = 3000
+
+app.set('view port', 'ejs')
+app.use(express.static('./assets'))
+
+// fire controllers
+todoController(app)
+
+
+app.listen(port, () => console.log(`Server running at http://127.0.0.1:${port}/`))
